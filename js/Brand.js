cc1c357f6c1d84fc237de6477550efb88dd088d3
Sam Reid <reids@colorado.edu>
2014-04-14 14:16:33 -0600

    Added brand/js/ in master to resolve the build issue identified in #1

---
+++ js/Brand.js
@@ -0,0 +1,9 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+// Returns branding information for the simulations, see https://github.com/phetsims/brand/issues/1
+define( function() {
+  'use strict';
+
+  //TODO: Add the name of your group here.
+  return function() { return {name: ''}; };
+} );
