a1db1d7f469746c2f4a387627c6b846e77109403
Sam Reid <reids@colorado.edu>
2014-04-14 13:37:00 -0600

    Added brand/js/ to resolve the build issue identified in #1

---
+++ js/Brand.js
@@ -0,0 +1,7 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+// Returns branding information for the simulations, see https://github.com/phetsims/brand/issues/1
+define( function() {
+  'use strict';
+  return function() { return {name: 'PhET Interactive Simulations'}; };
+} );
