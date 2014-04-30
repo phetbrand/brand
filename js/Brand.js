61a6c4ae08b0e6e556a6873274573bc42ffcd8de
Sam Reid <reids@colorado.edu>
2014-04-30 10:48:44 -0600

    Updated name for PhET brand

--- js/Brand.js
+++ js/Brand.js
@@ -3,5 +3,5 @@
 // Returns branding information for the simulations, see https://github.com/phetsims/brand/issues/1
 define( function() {
   'use strict';
-  return function() { return {name: 'PhET Interactive Simulations'}; };
+  return {name: 'PhET Interactive Simulations'};
 } );
