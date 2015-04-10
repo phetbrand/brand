1bace7c37800831401360294ff39cd51ae0e6af4
Sam Reid <reids@colorado.edu>
2015-04-09 18:57:46 -0600

    Added custom wording for the About Dialog for together sims, see https://github.com/phetsims/brand/issues/3 and https://github.com/phetsims/together/issues/60

--- js/Brand.js
+++ js/Brand.js
@@ -7,16 +7,16 @@ define( function( require ) {
   var phetString = 'PhET Interactive Simulations'; // as in Java sims, do not internationalize
   var copyrightString = 'Copyright © 2004-2015 University of Colorado Boulder'; // as in Java sims, do not internationalize
 
-  var termsPrivacyAndLicensingString = require( 'string!JOIST/termsPrivacyAndLicensing' );
+  var licenseString = require( 'string!JOIST/license' );
 
   // constants
-  var SOFTWARE_LICENSING_URL = 'http://phet.colorado.edu/en/html-terms-privacy-and-licensing';
+  var SOFTWARE_LICENSING_URL = 'https://github.com/phetsims/together/blob/master/LICENSE';
 
   return {
     name: phetString,
     copyright: copyrightString,
     links: [
-      { text: termsPrivacyAndLicensingString, url: SOFTWARE_LICENSING_URL }
+      { text: licenseString, url: SOFTWARE_LICENSING_URL }
     ]
   };
 } );
